60aacf4ad3ecf3562443b481aacd8f3c01c78c1a
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-12 22:42:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("hello")
+fullname = input("what is you full name? ")
+age = input("how old are you? ")
+favorite_act = input("what is your favorite activity? ")
+
+#Concatination 
+print("your name is " + fullname)
+print("Your are", age, " years old")
+print("Your favorite activity is " + favorite_act)
