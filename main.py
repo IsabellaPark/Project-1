f4cb3f4291bfcb248d18e765af576074420508cd
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-12 23:02:31 +0000

    a project lol

--- main.py
+++ main.py
@@ -10,8 +10,20 @@
  
 #--------------------------------------------------------
 #data convertions 
-var1 = 1 #integer value
-var_string = str(var1)
-print(var1)
-print(var_string)
+# var1 = 1 #integer value
+# var_string = str(var1)
+# print(var1)
+# print(var_string)
+
+#---------------------------------------------------------
+#get perimeter of a rectangle 
+#User inputs length and width 
+#console outputs perimeter 
+
+perimeter = input("enter a perimeter of a rectangle: ")
+l_w = input("enter the length and width of the rectangle from the primeter (l, w:)")
+print("the perimeter of the rectangle is " + perimeter)
+print("the length and width of the rectangle is " + l_w)
+
+
 
