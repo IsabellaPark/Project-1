22e5c80376b70cecb4f4a98b45544a059dec26bf
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-12 22:48:00 +0000

    string and var

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-print("hello")
-fullname = input("what is you full name? ")
-age = input("how old are you? ")
-favorite_act = input("what is your favorite activity? ")
+# print("hello")
+# fullname = input("what is you full name? ")
+# age = input("how old are you? ")
+# favorite_act = input("what is your favorite activity? ")
 
-#Concatination 
-print("your name is " + fullname)
-print("Your are", age, "years old")
-print("Your favorite activity is " + favorite_act)
+# #Concatination 
+# print("your name is " + fullname)
+# print("Your are", age, "years old")
+# print("Your favorite activity is " + favorite_act)
+ 
+#--------------------------------------------------------
+#data convertions 
+var1 = 1 #integer value
+var_string = str(var1)
+print(var1)
+print(var_string)
+
